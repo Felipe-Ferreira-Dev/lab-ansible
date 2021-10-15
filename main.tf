da2f59fc6c32f47513251c7cb6e6b8c99fcac0a4
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-15 14:42:27 -0300

    role port 22

--- main.tf
+++ main.tf
@@ -27,6 +27,17 @@ resource "aws_security_group" "allow_ssh_terraform" {
   ingress = [
     {
       description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "SSH from VPC"
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
